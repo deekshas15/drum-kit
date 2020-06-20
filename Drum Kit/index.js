40426c882c7426ac4119298ed574b425684073d2
deekshas15 <64957719+deekshas15@users.noreply.github.com>
2020-06-20 22:46:05 +0530

    adding sounds and html,css and java script code

---
+++ Drum Kit/index.js
@@ -0,0 +1,64 @@
+// Detecting click
+
+var drums=document.querySelectorAll(".drum");
+for(var i=0;i<drums.length;i++){
+  drums[i].addEventListener("click", function(){
+    var drumType=this.innerText;
+    pressSound(drumType);
+    buttonAnimation(drumType);
+  });
+}
+
+// Detecting keyboard press
+
+document.addEventListener("keydown",function(event){
+    pressSound(event.key);
+    buttonAnimation(event.key);
+    });
+
+// making sound
+
+function pressSound(key){
+switch (key) {
+  case "w":
+    var tom1=new Audio("sounds/tom-1.mp3");
+    tom1.play();
+  break;
+  case "a":
+    var tom2=new Audio("sounds/tom-2.mp3");
+    tom2.play();
+  break;
+  case "s":
+    var tom3=new Audio("sounds/tom-3.mp3");
+    tom3.play();
+  break;
+  case "d":
+    var tom4=new Audio("sounds/tom-4.mp3");
+    tom4.play();
+  break;
+  case "j":
+    var snare=new Audio("sounds/snare.mp3");
+    snare.play();
+  break;
+  case "k":
+    var kick=new Audio("sounds/kick-bass.mp3");
+    kick.play();
+  break;
+  case "l":
+    var crash=new Audio("sounds/crash.mp3");
+    crash.play()
+  break;
+
+  default:console.log("nopes")
+
+}
+}
+
+
+function buttonAnimation(currentKey){
+  document.querySelector("."+currentKey).classList.add("pressed");
+
+  setTimeout(function(){
+    document.querySelector("."+currentKey).classList.remove("pressed");
+  },100);
+}
